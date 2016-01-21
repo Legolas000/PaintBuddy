e6d56a334a2d8d94988fc5d97a1ff74bc0a60e44
Legolas000 <gsinthujan@yahoo.com>
2016-01-21 20:49:21 +0530

    Added Functionality
    Added change status to orders page

---
+++ app/Http/Controllers/Artist/ArtsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Artist;
+
+use App\Orders;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
+
+class ArtsController extends Controller
+{
+    //
+    public function ViewAOrders()
+    {
+        $order = Orders::all();
+        return view('pages/Artist/artOrders')->with('order',$order);
+        //return 'hello';
+    }
+
+    public function ViewCOrders()
+    {
+        $order = Orders::where('status','=','Completed')->get();
+        return view('pages/Artist/artOrders')->with('order',$order);
+    }
+
+    public function ViewOOrders()
+    {
+        $order = Orders::where('status','=','Ongoing')->get();
+        return view('pages/Artist/artOrders')->with('order',$order);
+    }
+
+    public function ViewCal()
+    {
+        $order = Orders::all();
+        return view('pages/Artist/ordCalendar')->with('order',json_encode($order));
+    }
+
+    public function UpdOrderStat($ordID)
+    {
+        Orders::where('ordID',$ordID)
+            ->update(['status' => 'Completed']);
+        return Redirect::to('/ArtMainOrders');
+    }
+}
