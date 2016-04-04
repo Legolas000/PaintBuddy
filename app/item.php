2d8015e036ae20479fbc7d30b92cd5bd4607be30
Legolas000 <gsinthujan@yahoo.com>
2016-04-04 18:28:37 +0530

    Cleaned repository..
    Repository completely cleaned

--- app/item.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-class Item extends BaseModel {
-    protected $primaryKey = 'itID';
-    protected $table = 'items';
-    protected $fillable = array('itName', 'itDescrip', 'imPath','itSize','catRef','price','status');
-}
--- resources/views/pages/Artist/MailPages/Mail1.blade.php
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-{{--This file is for reference purposes. Not used in this project--}}
-=======
->>>>>>> origin/master
-<p>The item {!! $itemName !!} which you have ordered, has had its price updated to {!! $price !!}. Would you like to
-    have this delivered? Awaiting you reply
-    Your Sincerely,
-    PainBuddy Team</p>
