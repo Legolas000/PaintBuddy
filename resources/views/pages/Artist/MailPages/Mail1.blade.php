9fb9ad93fc3df8548b06b9a07869de816b761fea
Legolas000 <gsinthujan@yahoo.com>
2016-04-05 12:52:09 +0530

    Removed repo
    Removed project since there were errors

--- resources/views/pages/Artist/MailPages/Mail1.blade.php
+++
@@ -1,5 +0,0 @@
-{{--This file is for reference purposes. Not used in this project--}}
-<p>The item {!! $itemName !!} which you have ordered, has had its price updated to {!! $price !!}. Would you like to
-    have this delivered? Awaiting you reply
-    Your Sincerely,
-    PainBuddy Team</p>
