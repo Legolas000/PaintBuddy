8fdcce6dafba3fab2ce79562ee5e9ba19d90503e
Legolas000 <gsinthujan@yahoo.com>
2016-04-05 13:13:23 +0530

    Repo cleaned - Take 3

--- app/category.php
+++
@@ -1,7 +0,0 @@
-<?php namespace App;
-
-class Category extends BaseModel {
-    protected $primaryKey = 'catID';
-    protected $table = 'categories';
-    protected $fillable = array('catName');
-}
