0a3b0b964f27d114f421f61eb9e22dd59ae43ce3
Arhamshan <arhamshan625@gmail.com>
2016-05-03 16:28:20 +0530

    Update viewPromotion8.blade.php
    display all the promotion at same time

--- resources/views/pages/Discount/viewPromotion8.blade.php
+++ resources/views/pages/Discount/viewPromotion8.blade.php
@@ -1,65 +1,44 @@
 @extends('layout.layout')
 @section('content')
 
-
-   	<div class="form-group">
-
-
-        
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <form role="form" action="/enterpromotion"  method="POST">
-                        <div class="form-group " class="col-md-6">
-                            <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-			                    <img src="images\promotion\1.png" style="height: 250px;width: 600px; margin-left:280px" class="img-rounded"/>
-			                <div class="col-md-12">
-			                	<label class="col-md-12" style="color:#00997a; font-size:30px; text-align:center " ><b>
-                				<marquee scrollamount="5" width="70">&lt;&lt;&lt;&lt;&lt;</marquee>{{ $titl->title }}<marquee scrollamount="5" direction="right" width="70">&gt;&gt;&gt;&gt;&gt;</marquee>
-                				
-                                </b>
-                			</label>
-                			<br/><br/>
-                			<p style="font-size:20px; color:#006652; text-align:center">
-                				{{ $boddy->body }}
-                                The ptomotion will be available from {{ $sdat->sdate }} to {{ $edat->edate }} Please Enter to register for promotion......!!!
-                               
-
-                			</p>
-                			</div>
-                			<div class="form-group" class="col-md-6" style="text-align:center">
-                			
-                            <br/>
-                			<button type="submit" class="btn btn-primary" >Enter </button>
-
-                            <!-- <div class="button-group"> 
-                			<a href="/registerpromotion">Enter </a>
+<div class="form-group">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+            <div class="panel panel-default, col-md-12 col-md-offset-0 ">
+                <form role="form" action="/enterpromotion"  method="POST">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                    <div class="form-group " class="col-md-6">
+                        <img src="images\promotion\1.png" style="height: 250px;width: 600px; margin-left:280px" class="img-rounded"/>
+                        <p style="text-align:center; font-size:25px; color:#00b3b3"><b><u> WelCome to the Promotion...!!! You can register only one promotion at a time.</u></b></p>
+                        
+                        <div class="row">
+                        @foreach($promotiontable as $promotiontable1)
+                            <div class="col-sm-6"> 
+                                <div class="panel panel-default"> 
+                                    <div style="text-align:center ">  
+                                        <label style="color:#800000; font-size:20px" ><b>
+                                            <marquee scrollamount="5" width="70">&lt;&lt;&lt;</marquee>{{ $promotiontable1->title }}<marquee scrollamount="5" direction="right" width="70">&gt;&gt;&gt;</marquee></b>
+                                        </label>
+                                    </div>
+                                    <br/>
+                                    <p style="font-size:15px; color:#99003d; text-align:center">
+                                        {{ $promotiontable1->body }}
+                                         The ptomotion will be available from {{ $promotiontable1->sdate }} to {{ $promotiontable1->edate }} Please Enter to register for promotion......!!!
+                                    </p>
+                                    <div style="text-align:center">
+                                    <button type="submit" class="btn btn-primary" >Enter </button>
+                                    </div><br/>
+                                </div>
                             </div>
- -->
-
-                            
-
-                			<!--<script src="js/sweetalert.min.js"></script>
-
-                			<script >
-                				swal({
-                					title:'Great Job',
-                					type:'success'
-                				});
-
-                			</script>
-                			-->
-
-
-
-
-
-
-
+                            @endforeach
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
+</div>
+
 @stop
