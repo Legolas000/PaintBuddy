458bde8bed1186ac049ffc6d6f9b6494e119c854
Arhamshan <arhamshan625@gmail.com>
2016-05-03 16:17:57 +0530

    Update discountpage28.blade.php

--- resources/views/pages/Discount/discountpage28.blade.php
+++ resources/views/pages/Discount/discountpage28.blade.php
@@ -34,30 +34,34 @@
                                 </p>
                                 <hr/>
                             </div>        
-                            @if($oldDiscount >= 1)
-                                <link rel="stylesheet" type="text/css" href="css/sweetalert.css">
-                                <script src="js/sweetalert.min.js"></script>
+                            @if($oldDiscount >= 1) 
+                                <link rel="stylesheet" type="text/css" href="assets/sweetalert/dist/sweetalert.css">
+                                <script src="assets/sweetalert/dist/sweetalert.min.js"></script>
                                 <script>
                                     sweetAlert("Notice..!!!", "A discount is expiered on today");
                                 </script>
                             @endif
                                 <label for="InputItem">
-                                     Item Category
+                                     Items
                                 </label>
-                            </div>
-                            <select name="itmcat" class="form-control">
-                            @foreach($item as $user) 
-                            <option>{{ $user->itName }}</option>
-                            @endforeach
-                            </select> 
-                            <div>
-                                <br/>
-                            </div>
-                             <div class="form-group" class="col-xs-3">
+                                <div>
+                                    <br/>
+                                </div>
+                                <select name="itmcat" class="form-control">
+                                    @foreach($item as $user) 
+                                        <option>{{ $user->itName }}</option>
+                                    @endforeach
+                                </select> 
+                                <div>
+                                    <br/>
+                                </div>
+                            <div class="form-group" class="col-xs-3">
                                 <label for="InputItem">
                                     Discount Type
                                 </label>
-                            <div class="form-group">
+                                <div>
+                                    <br/>
+                                </div>
                                 <select name="disctyp"  class="form-control">
                                     <option> Seasonal discount </option>
                                     <option> Festival discount </option>
@@ -65,16 +69,22 @@
                                     <option> Year end discount</option>
                                     <option> Other types </option>
                                 </select>
-                            </div></div>
+                            </div>
                             <div>
                                 <br/>
                             </div>
                             <div class="form-group" class="col-xs-3">
                                 <label for="InputPercentage">
-                                    Percentage 
+                                    Percentage
                                 </label>
+                                <div>
+                                    <br/>
+                                </div>
                                 <input type="text" name="Percentage" class="form-control" placeholder="%" id="InputPrice"/> <!-- required pattern="\d{1,2}"/> -->
                                <P style="color:red">{{$errors->first('Percentage')}} </P>
+                            </div>
+                            <div>
+                                <br/>
                             </div>
                             <div >
                                 <label for="startDate"> 
@@ -82,109 +92,116 @@
                                 </label>
                             </div>
                             <div class='input-group date' id="dp1">
-                              <input type="date" name="start_date" class="form-control"/>
-                              <P style="color:red">{{$errors->first('start_date')}}</P>
+                                <div>
+                                    <br/>
+                                </div>
+                                <input type="date" name="start_date" class="form-control"/>
+                                <P style="color:red">{{$errors->first('start_date')}}</P>
                             </div>
-                            <div><br/></div>
                             <div>
-                                <label for="endDate" >
-                                    End Date
+                                <br/>
+                               <label for="endDate" >
+                                    End Date ddd
                                 </label>
+                            </div>
+                            <div>
+                                <br/>
                             </div>
                             <div class='input-group date'  id="dp2">
                                 <input type="date" name="end_date" class="form-control"/>
                                 <P style="color:red">{{$errors->first('end_date')}}</P>
                             </div>
+                            <div>
+                                <br/>
+                            </div>
                             <div class="form-group" class="col-md-6">
-                            <div><br/>
-                                <button type="submit" class="btn btn-primary"  class="icon-search icon-white"  >Add Discount</button><br/> 
+                                <button type="submit" class="btn btn-primary" lass="icon-search icon-white">Add Discount</button><br/> 
                                 @if($id == 0)
-                                <link rel="stylesheet" type="text/css" href="css/sweetalert.css">
-                                <script src="js/sweetalert.min.js"></script>
-                                <script>
-                                    sweetAlert("Sorry..!!!", "You have already added a discount for this item", "error");
-                                </script>
+                                    <link rel="stylesheet" type="text/css" href="assets/sweetalert/dist/sweetalert.css">
+                                    <script src="assets/sweetalert/dist/sweetalert.min.js"></script>
+                                    <script>
+                                        sweetAlert("Sorry..!!!", "You have already added a discount for this item", "error");
+                                    </script>
                                 @endif
                                 @if($td == 1)
-                                <link rel="stylesheet" type="text/css" href="css/sweetalert.css">
-                                <script src="js/sweetalert.min.js"></script>
-                                <script>
-                                    sweetAlert("Success..!!!", "You have successfuly added a discount", "success");
-                                </script>
+                                    <link rel="stylesheet" type="text/css" href="assets/sweetalert/dist/sweetalert.css">
+                                    <script src="assets/sweetalert/dist/sweetalert.min.js"></script>
+                                    <script>
+                                        sweetAlert("Success..!!!", "You have successfuly added a discount", "success");
+                                    </script>
                                 @endif
-
                             </div>
                         </div>
                     {{ Form::close() }}
                 </div>
                 <form role="form" action="/viewDiscount" method="POST">   
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                     <div class="form-group col-md-offset-0" class="col-md-6">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                         <div class="col-md-7">
                             <button class="btn btn-primary" >View Discount </button>   
                         </div> <br/>    
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-12">
-                            <br/>
-                            <table class="table table-bordered table-hover" >
-                                <thead>
-                                    <tr class="info">
-                                        <th class="col-md-2 text-center">
-                                            Discount ID
-                                        </th>
-                                        <th class="col-md-2 text-center">
-                                            Item Name
-                                        </th>
-                                        <th class="col-md-2 text-center">
-                                            Discount Type
-                                        </th>
-                                        <th class="col-md-1 text-center">
-                                            Percentage
-                                        </th>                                            
-                                        <th class="col-md-2 text-center">
-                                            Discount Price
-                                        </th>
-                                        <th class="col-md-2 text-center">
-                                            Start Date
-                                        </th>
-                                        <th class="col-md-2 text-center">
-                                            End Date
-                                        </th>  
-                                    </tr>   
-                                </thead>
-                                    <tbody>
-                                        <?php 
-                                            if (isset($table2)) {
-                                               foreach( $table2 as $row ){
-                                        ?>
-                                        <tr class="success">
-                                            <td><?php echo $row->did; ?>  </td>
-                                            <td><?php echo $row->iname; ?> </td>
-                                            <td><?php echo $row->dtype; ?></td>
-                                            <td><?php echo $row->ipersentage; ?></td>
-                                            <td><?php echo $row->iprice; ?></td>
-                                            <td><?php echo $row->sdate; ?></td>
-                                            <td><?php echo $row->edate; ?></td>
-                                            <!--  <td><a href="testmesage" class="btn btn-primary"> MESAGE</button> </td>-->            
-                                        </tr>
-                                        <?php } }?>
-                                    </tbody>
-                                </table>
-                                @if(is_null($values))
-                                    <label style="color:red">
-                                        <b><I>  No discounts available...!!!</I></b>
-                                    </label>
-                                @endif
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-12">
+                                <br/>
+                                <table class="table table-bordered table-hover" >
+                                    <thead>
+                                        <tr class="info">
+                                            <th class="col-md-2 text-center">
+                                                Discount ID
+                                            </th>
+                                            <th class="col-md-2 text-center">
+                                                Item Name
+                                            </th>
+                                            <th class="col-md-2 text-center">
+                                                Discount Type
+                                            </th>
+                                            <th class="col-md-1 text-center">
+                                                Percentage
+                                            </th>                                            
+                                            <th class="col-md-2 text-center">
+                                                Discount Price
+                                            </th>
+                                            <th class="col-md-2 text-center">
+                                                Start Date
+                                            </th>
+                                            <th class="col-md-2 text-center">
+                                                End Date
+                                            </th>  
+                                        </tr>   
+                                    </thead>
+                                        <tbody>
+                                            <?php 
+                                                if (isset($table2)) {
+                                                    foreach( $table2 as $row ) {
+                                            ?>
+                                                        <tr class="success">
+                                                            <td><?php echo $row->did; ?>  </td>
+                                                            <td><?php echo $row->iname; ?> </td>
+                                                            <td><?php echo $row->dtype; ?></td>
+                                                            <td><?php echo $row->ipersentage; ?></td>
+                                                            <td><?php echo $row->iprice; ?></td>
+                                                            <td><?php echo $row->sdate; ?></td>
+                                                            <td><?php echo $row->edate; ?></td>
+                                                            <!--  <td><a href="testmesage" class="btn btn-primary"> MESAGE</button> </td>-->            
+                                                        </tr>
+                                            <?php   } 
+                                                } ?>
+                                        </tbody>
+                                    </table>
+                                    @if(is_null($values))
+                                        <label style="color:red">
+                                            <b><I>  No discounts available...!!!</I></b>
+                                        </label>
+                                    @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </div>
-
 
 @stop
